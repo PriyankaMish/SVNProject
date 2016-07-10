5a8c37972ffd70c800cbe7ca651279556b81b102
Priyanka Mishra <priyankamishra8m@gmail.com>
2016-07-10 14:42:52 +0530

    Updated With *****

--- src/qsp/Demo1.java
+++ src/qsp/Demo1.java
@@ -4,11 +4,19 @@ public class Demo1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hiiiiiii");
+		System.out.println("*********************");
 		System.out.println("Hiiiiiii");
+		System.out.println("*********************");
 		System.out.println("Hiiiiiii");
+		System.out.println("*********************");
 		System.out.println("Hiiiiiii");
+		System.out.println("*********************");
 		System.out.println("Hiiiiiii");
+		System.out.println("*********************");
 		System.out.println("Hiiiiiii");
+		System.out.println("*********************");
+		
+		
 	}
 
 }
